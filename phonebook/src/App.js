754ea2953ca892a150519fc130c4ecfa618cb0a7
Akbermet <akbermet.zh@gmail.com>
2021-10-31 21:25:08 +0600

    2.9*: The Phonebook Step4, everything works

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,23 @@
 import React, { useState } from 'react'
 
+const Filter = ({persons, filterPattern}) => {
+  const filtering = (contact) => {
+    const contactName = contact.name.toLowerCase()
+  
+    if(contactName.indexOf(filterPattern.toLowerCase()) !== -1 && filterPattern !== "")
+    {
+      return true
+    }
+  }
+
+  const filteredContactList = persons.filter(filtering)
+  console.log("filtered: ", filteredContactList)
+
+  return(
+    <Contacts persons={filteredContactList}/>
+  )
+}
+
 const Contacts = ({persons}) => {
   return(
     <div>
@@ -12,8 +30,10 @@ const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', phoneNumb: '706881177' }
   ]) 
+
   const [ newName, setNewName ] = useState('')
   const [newPhoneNumb, setNewPhoneNumb ] = useState('')
+  const [filterPattern, setFilterPattern ] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -31,7 +51,6 @@ const App = () => {
   
       console.log("obj that's adde to the persons state", nameObject)
       setPersons(persons.concat(nameObject))
-      // setNewName("")
     }
     else
     {
@@ -47,13 +66,18 @@ const App = () => {
     setNewPhoneNumb(event.target.value)
   }
 
-  // const addPhoneNumb = (event) => {
-  //   event.preventDefault()
-  // }
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setFilterPattern(event.target.value)
+  } 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with: <input type="text" value={filterPattern} onChange={handleFilterChange} /></div>
+      <Filter persons={persons} filterPattern={filterPattern} />
+
+      <h2> add a new </h2>
       <form onSubmit={addContact}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newPhoneNumb} onChange={handlePhoneNumbChange}/></div>
@@ -62,9 +86,6 @@ const App = () => {
 
       <h2>Numbers</h2>
       <Contacts persons={persons}/>
-
-      <div>debug: {newName} {newPhoneNumb} </div>,
-      {console.log(persons)}
     </div>
   )
 }
