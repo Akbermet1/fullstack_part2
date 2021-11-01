7307d5cc16345140235fbee1158bdc89346ea8e6
Akbermet <akbermet.zh@gmail.com>
2021-11-01 18:03:45 +0600

    2.10: The Phonebook Step5, everything works

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react'
 
+const PersonForm = ({handleSubmit, input1_info, input2_info, button_info}) => {
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <div> {input1_info.text} <input value={input1_info.state} onChange={input1_info.eventHandler} /></div>
+      <div> {input2_info.text} <input value={input2_info.state} onChange={input2_info.eventHandler}/></div>
+      <div> <button type={button_info.type}> {button_info.text} </button> </div>
+    </form>
+  )
+}
+
 const Filter = ({persons, filterPattern}) => {
   const filtering = (contact) => {
     const contactName = contact.name.toLowerCase()
@@ -71,18 +82,26 @@ const App = () => {
     setFilterPattern(event.target.value)
   } 
 
+  const input1_details = {
+    text: "name:",
+    state: newName,
+    eventHandler: handleNameChange
+  }
+
+  const input2_details = {
+    text: "number: ",
+    state: newPhoneNumb,
+    eventHandler: handlePhoneNumbChange
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>filter shown with: <input type="text" value={filterPattern} onChange={handleFilterChange} /></div>
       <Filter persons={persons} filterPattern={filterPattern} />
 
-      <h2> add a new </h2>
-      <form onSubmit={addContact}>
-        <div>name: <input value={newName} onChange={handleNameChange}/></div>
-        <div>number: <input value={newPhoneNumb} onChange={handlePhoneNumbChange}/></div>
-        <div> <button type="submit">add</button> </div>
-      </form>
+      <h2> Add a new </h2>
+      <PersonForm handleSubmit={addContact} input1_info={input1_details} input2_info={input2_details} button_info={{type: "submit", text: "add"}}/>
 
       <h2>Numbers</h2>
       <Contacts persons={persons}/>
